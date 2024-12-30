afef2e7377e432804d4cdb803d7194fd41b8a5a2
Dilip Rajput <mail2diliprajput@gmail.com>
2024-12-30 23:30:16 +0530

    lerarnig github code

--- Terraform-starline-code-main/Terraform-starline-code-main/vpc_ec2/main.tf
+++ Terraform-starline-code-main/Terraform-starline-code-main/vpc_ec2/main.tf
@@ -19,3 +19,9 @@ module "volume" {
     source = "../vpc_ec2/modules/ec2/main.tf"
     instance = var.instance_type
 }
+
+
+module "voTATATATATATATATATATTlume" {
+    source = "../vpc_ec2/modules/ec2/main.tf"
+    instance = var.instance_type
+}
